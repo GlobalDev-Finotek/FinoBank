4eb70f0bfbec0cd01008ab50ba13edb80a2f4630
cailhuiris <cailhuiris@gmail.com>
2017-03-20 17:24:18 +0900

    updated SplashActivity.java, and also created MainActivity

--- app/src/main/java/finotek/global/dev/talkbank_ca/SplashActivity.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/SplashActivity.java
@@ -5,6 +5,7 @@ import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -16,41 +17,24 @@ import java.util.concurrent.TimeUnit;
 
 import finotek.global.dev.talkbank_ca.chat.ChatActivity;
 import finotek.global.dev.talkbank_ca.databinding.ActivitySplashBinding;
+import rx.Observable;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class SplashActivity extends AppCompatActivity {
-  private ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.activity_splash);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
-    binding.tvIntro.setText(R.string.fintek_intro);
-
-
-    if (true) {
-      binding.btnNext.setText(R.string.string_user_regi);
-    } else {
-      binding.btnNext.setText(R.string.string_login);
+        Observable.timer(3, TimeUnit.SECONDS)
+            .subscribe(aVoid -> {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            });
     }
-
-
-    RxView.clicks(binding.btnNext)
-        .throttleFirst(200, TimeUnit.MILLISECONDS)
-        .subscribe(aVoid -> {
-
-          Intent intent;
-
-          if (true) {
-            intent = new Intent(SplashActivity.this, ChatActivity.class);
-          } else {
-            intent = new Intent(SplashActivity.this, ChatActivity.class);
-          }
-
-          startActivity(intent);
-        });
-  }
 }

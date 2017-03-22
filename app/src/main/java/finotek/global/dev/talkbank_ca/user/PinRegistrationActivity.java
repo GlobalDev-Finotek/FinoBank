ada618534c37548066954f278d74b70cfbba2de7
Kyeongwook Ma <kyeongwook.ma@finotek.co.kr>
2017-03-22 17:35:23 +0900

    [UI 변경]

--- app/src/main/java/finotek/global/dev/talkbank_ca/user/PinRegistrationActivity.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/user/PinRegistrationActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,26 +29,21 @@ public class PinRegistrationActivity extends AppCompatActivity {
 	private final int PINCODE_LENGTH = 6;
 	ActivityPinRegistrationBinding binding;
 	private TextView[] tvPwd = new TextView[PINCODE_LENGTH];
+
+
 	private int ptrTvPwd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		binding = DataBindingUtil.setContentView(this, R.layout.activity_pin_registration);
+		setSupportActionBar(binding.toolbar);
+		getSupportActionBar().setTitle("PIN 코드");
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		for(Pin.Color c : Pin.Color.values()) {
-			TextView iv = (TextView) crateColoredView(c);
-			binding.glPinBackground.addView(iv);
-			iv.setOnClickListener(v -> binding.glPinCode.setBackgroundColor(iv.getCurrentTextColor()));
-
-			TextView iv2 = (TextView) crateColoredView(c);
-			binding.glPinTextColor.addView(iv2);
-
-			iv2.setOnClickListener(v -> {
-				for(TextView tv : tvPwd) {
-					tv.setTextColor(iv2.getCurrentTextColor());
-				}
-			});
+		for (Pin.Color c : Pin.Color.values()) {
+			setPinBackgroundCircle(c);
+			setTextColorCircle(c);
 		}
 
 		binding.glPinCode.setUseDefaultMargins(false);
@@ -54,8 +51,9 @@ public class PinRegistrationActivity extends AppCompatActivity {
 		binding.glPinCode.setRowOrderPreserved(false);
 
 		for (int i = 0; i < PINCODE_LENGTH; ++i) {
+
 			TextView tv = new TextView(this);
-			tv.setPadding(50, 20, 50, 20);
+			tv.setPadding(60, 20, 60, 20);
 			tv.setTextSize(40f);
 			tv.setText(" ");
 			tv.setBackground(ContextCompat.getDrawable(this, R.drawable.border_black_blank));
@@ -83,17 +81,42 @@ public class PinRegistrationActivity extends AppCompatActivity {
 				tvPwd[ptrTvPwd++].setText("*");
 			}
 
-			if (position == 7) {
-				if (ptrTvPwd > 0) {
-					tvPwd[--ptrTvPwd].setText(" ");
-				}
-			} else if (position == secureKeyboardAdapter.getCount() - 1) {
+			if (position == secureKeyboardAdapter.getCount() - 1) {
 				startActivity(new Intent(PinRegistrationActivity.this, SettingsActivity.class));
 			}
 
 		});
 
+		secureKeyboardAdapter.setOnBackPressListener(() -> {
+			if (ptrTvPwd > 0) {
+				tvPwd[--ptrTvPwd].setText(" ");
+			}
+		});
 
+
+	}
+
+	private void setTextColorCircle(Pin.Color c) {
+		ImageView iv2 = new ImageView(this);
+		iv2.setTag(ContextCompat.getColor(this, c.getColor()));
+		iv2.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.circle));
+		DrawableCompat.setTint(iv2.getDrawable(), ContextCompat.getColor(this, c.getColor()));
+		binding.glPinTextColor.addView(iv2);
+
+		iv2.setOnClickListener(v -> {
+			for (TextView tv : tvPwd) {
+				tv.setTextColor((int) iv2.getTag());
+			}
+		});
+	}
+
+	private void setPinBackgroundCircle(Pin.Color c) {
+		ImageView iv = new ImageView(this);
+		iv.setTag(ContextCompat.getColor(this, c.getColor()));
+		iv.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.circle));
+		DrawableCompat.setTint(iv.getDrawable(), ContextCompat.getColor(this, c.getColor()));
+		binding.glPinBackground.addView(iv);
+		iv.setOnClickListener(v -> binding.glPinCode.setBackgroundColor((int) iv.getTag()));
 	}
 
 	private List<String> getCompleteRandomizedSeq() {
---
+++ app/src/main/java/finotek/global/dev/talkbank_ca/widget/SignRegistrationProcessingDialog.java
@@ -0,0 +1,16 @@
+package finotek.global.dev.talkbank_ca.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by magyeong-ug on 2017. 3. 22..
+ */
+
+public class SignRegistrationProcessingDialog extends Dialog {
+
+	public SignRegistrationProcessingDialog(@NonNull Context context) {
+		super(context);
+	}
+}

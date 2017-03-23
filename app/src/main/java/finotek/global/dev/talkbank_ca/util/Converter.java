c62eecd182d2aa312aef75b8760de60f1c159016
cailhuiris <cailhuiris@gmail.com>
2017-03-23 13:49:32 +0900

    updated

--- app/src/main/java/finotek/global/dev/talkbank_ca/util/Converter.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/util/Converter.java
@@ -4,9 +4,14 @@ import android.content.res.Resources;
 import android.util.DisplayMetrics;
 
 public class Converter {
-    public static float convertPixelsToDp(float px){
+    public static int pxToDp(int px){
         DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
-        float dp = px / (metrics.densityDpi / 160f);
+        int dp = (int) (px / (metrics.densityDpi / 160f));
         return Math.round(dp);
+    }
+
+    public static int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 }

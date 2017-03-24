2755f369d690812661b573e617f0db745c30df90
Kyeongwook Ma <kyeongwook.ma@finotek.co.kr>
2017-03-24 11:04:01 +0900

    [기능 수정] 설정 페이지 재구성

--- app/src/main/java/finotek/global/dev/talkbank_ca/setting/SettingsActivity.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/setting/SettingsActivity.java
@@ -1,12 +1,17 @@
 package finotek.global.dev.talkbank_ca.setting;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+
+import com.jakewharton.rxbinding.view.RxView;
+
+import java.util.concurrent.TimeUnit;
 
 import finotek.global.dev.talkbank_ca.R;
 import finotek.global.dev.talkbank_ca.databinding.ActivitySettingsBinding;
+import finotek.global.dev.talkbank_ca.user.UserRegistrationActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -21,29 +26,31 @@ public class SettingsActivity extends AppCompatActivity {
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setDisplayShowHomeEnabled(true);
 		binding.toolbar.setNavigationOnClickListener(view -> onBackPressed());
-		binding.tabs.addTab(binding.tabs.newTab().setText("사용자 정보"));
-		binding.tabs.addTab(binding.tabs.newTab().setText("맥락 인증"));
-		binding.tabs.addTab(binding.tabs.newTab().setText("금액별 인증"));
-		binding.tabs.addTab(binding.tabs.newTab().setText("이상거래 시 인증"));
 
-		SettingPageAdapter pageAdapter = new SettingPageAdapter(getFragmentManager());
-		binding.viewpager.setAdapter(pageAdapter);
-		binding.viewpager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(binding.tabs));
-		binding.tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-			@Override
-			public void onTabSelected(TabLayout.Tab tab) {
-				binding.viewpager.setCurrentItem(tab.getPosition());
-			}
+		RxView.clicks(binding.llUserinfo)
+				.throttleFirst(200, TimeUnit.MILLISECONDS)
+				.subscribe(aVoid -> {
+					startActivity(new Intent(SettingsActivity.this, UserRegistrationActivity.class));
+					finish();
+				});
 
-			@Override
-			public void onTabUnselected(TabLayout.Tab tab) {
+		RxView.clicks(binding.llCa)
+				.throttleFirst(200, TimeUnit.MILLISECONDS)
+				.subscribe(aVoid -> {
+					// TODO 맥락 인증 액티비티 생성 및 연결
+				});
 
-			}
+		RxView.clicks(binding.llCost)
+				.throttleFirst(200, TimeUnit.MILLISECONDS)
+				.subscribe(aVoid -> {
+					// TODO 금액 별 인증 액티비티 생성 및 연결
+				});
 
-			@Override
-			public void onTabReselected(TabLayout.Tab tab) {
+		RxView.clicks(binding.llAbnormal)
+				.throttleFirst(200, TimeUnit.MILLISECONDS)
+				.subscribe(aVoid -> {
+					// TODO 이상 거래 시 인증 및 연결
+				});
 
-			}
-		});
 	}
 }

48e3947fbd4eb00062c916856dcf5024fd84943f
마경욱 <kyeongwook.ma@liveo.me>
2017-04-03 13:34:58 +0900

    [기능 구현] 카메라 촬영 시 확대 축소 기능 구현

--- app/src/main/java/finotek/global/dev/talkbank_ca/user/CapturePicFragment.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/user/CapturePicFragment.java
@@ -186,6 +186,34 @@ public class CapturePicFragment extends Fragment
 	 */
 	private int mSensorOrientation;
 	private FragmentCapturePicBinding binding;
+	/**
+	 * {@link TextureView.SurfaceTextureListener} handles several lifecycle events on a
+	 * {@link TextureView}.
+	 */
+	private final TextureView.SurfaceTextureListener mSurfaceTextureListener
+			= new TextureView.SurfaceTextureListener() {
+
+		@RequiresApi(api = Build.VERSION_CODES.M)
+		@Override
+		public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
+			openCamera(width, height);
+		}
+
+		@Override
+		public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
+			configureTransform(width, height);
+		}
+
+		@Override
+		public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
+			return true;
+		}
+
+		@Override
+		public void onSurfaceTextureUpdated(SurfaceTexture texture) {
+		}
+
+	};
 	private boolean isCaptureDone;
 	private OnSizeChangeListener onSizeChangeListener;
 	/**
@@ -278,34 +306,6 @@ public class CapturePicFragment extends Fragment
 		@Override
 		public void onError(@NonNull CameraDevice cameraDevice, int error) {
 
-		}
-
-	};
-	/**
-	 * {@link TextureView.SurfaceTextureListener} handles several lifecycle events on a
-	 * {@link TextureView}.
-	 */
-	private final TextureView.SurfaceTextureListener mSurfaceTextureListener
-			= new TextureView.SurfaceTextureListener() {
-
-		@RequiresApi(api = Build.VERSION_CODES.M)
-		@Override
-		public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
-			openCamera(width, height);
-		}
-
-		@Override
-		public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
-			configureTransform(width, height);
-		}
-
-		@Override
-		public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
-			return true;
-		}
-
-		@Override
-		public void onSurfaceTextureUpdated(SurfaceTexture texture) {
 		}
 
 	};
@@ -430,12 +430,11 @@ public class CapturePicFragment extends Fragment
 					if (isFullSize) {
 						onSizeChangeListener.onSizeMinimize();
 						binding.ibSize.setImageDrawable(ContextCompat.getDrawable(getActivity(),
-								R.drawable.vector_drawable_icon_fullsize));
-
+								R.drawable.vector_drawable_icon_minimize));
 					} else {
 						onSizeChangeListener.onSizeFull();
 						binding.ibSize.setImageDrawable(ContextCompat.getDrawable(getActivity(),
-								R.drawable.vector_drawable_icon_minimize));
+								R.drawable.vector_drawable_icon_fullsize));
 					}
 
 					isFullSize = !isFullSize;

3014fd6ae454efa12ed2a1b58b7ba3e9f6ded8da
cailhuiris <cailhuiris@gmail.com>
2017-04-06 15:08:29 +0900

    커스텀 다이얼로그 버튼을 옵셔널로 변경

--- app/src/main/java/finotek/global/dev/talkbank_ca/user/dialogs/PrimaryDialog.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/user/dialogs/PrimaryDialog.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 
 import com.jakewharton.rxbinding.view.RxView;
@@ -44,6 +45,7 @@ public class PrimaryDialog extends Dialog {
     }
 
     public void setButtonText(String text){
+        binding.doneBtn.setVisibility(View.VISIBLE);
         binding.doneBtn.setText(text);
     }
 

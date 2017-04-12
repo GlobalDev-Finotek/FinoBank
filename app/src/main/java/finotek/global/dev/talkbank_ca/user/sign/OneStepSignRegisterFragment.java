d35dc90305c68cfb46ced7f858a2ec13c039aac9
마경욱 <kyeongwook.ma@liveo.me>
2017-04-12 11:16:27 +0900

    [기능 수정] 사인 한 번, 두 번에 따라 문구 변경

--- app/src/main/java/finotek/global/dev/talkbank_ca/user/sign/OneStepSignRegisterFragment.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/user/sign/OneStepSignRegisterFragment.java
@@ -15,7 +15,7 @@ import finotek.global.dev.talkbank_ca.R;
 public class OneStepSignRegisterFragment extends BaseSignRegisterFragment {
 	@Override
 	void initView() {
-		binding.tvInst.setText(getString(R.string.registration_string_fits_area_take_picture));
+		binding.tvInst.setText(getString(R.string.registration_string_please_sign_in));
 		binding.ibNext.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.btn_confirm_disable));
 	}
 

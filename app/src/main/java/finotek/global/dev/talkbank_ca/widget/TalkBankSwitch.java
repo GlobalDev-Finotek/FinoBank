db24c12d9faf0c95f762d8d968d7a4bbe888dab9
마경욱 <kyeongwook.ma@liveo.me>
2017-04-12 13:28:58 +0900

    [UI 수정] 스위치 외부 라이브러리로 대체

--- app/src/main/java/finotek/global/dev/talkbank_ca/widget/TalkBankSwitch.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/widget/TalkBankSwitch.java
@@ -6,7 +6,6 @@ import android.util.AttributeSet;
 import android.widget.Switch;
 
 import finotek.global.dev.talkbank_ca.R;
-import finotek.global.dev.talkbank_ca.util.Converter;
 
 /**
  * Created by magyeong-ug on 2017. 3. 30..
@@ -34,10 +33,14 @@ public class TalkBankSwitch extends Switch {
 	}
 
 	private void init(Context context) {
-		setMinimumHeight(Converter.dpToPx(30));
-		setSwitchMinWidth(Converter.dpToPx(58));
+
+		setSwitchMinWidth(40);
 		setTrackDrawable(ContextCompat.getDrawable(context, R.drawable.switch_track_off));
 		setThumbDrawable(ContextCompat.getDrawable(context, R.drawable.switch_thumb_selector));
+
 		setThumbTextPadding((int) context.getResources().getDimension(R.dimen.switch_thumb_radius));
+		setTextOn("On");
+		setTextOff("Off");
+
 	}
 }

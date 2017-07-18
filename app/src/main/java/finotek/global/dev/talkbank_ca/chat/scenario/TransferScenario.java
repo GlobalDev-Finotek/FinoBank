25387281661ff80d6e0b591be4f85c17435e53eb
Jungwon Seo <jungwonrs@gmail.com>
2017-07-18 17:59:46 +0900

    계좌 선택 시나리오

--- app/src/main/java/finotek/global/dev/talkbank_ca/chat/scenario/TransferScenario.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/chat/scenario/TransferScenario.java
@@ -1,8 +1,6 @@
 package finotek.global.dev.talkbank_ca.chat.scenario;
 
-import android.Manifest;
 import android.content.Context;
-import android.support.v4.app.ActivityCompat;
 
 import org.joda.time.DateTime;
 
@@ -34,7 +32,7 @@ import finotek.global.dev.talkbank_ca.chat.messages.transfer.alterOne;
 import finotek.global.dev.talkbank_ca.chat.messages.transfer.alterThree;
 import finotek.global.dev.talkbank_ca.chat.messages.transfer.alterTwo;
 import finotek.global.dev.talkbank_ca.chat.messages.ui.RequestRemoveControls;
-import finotek.global.dev.talkbank_ca.chat.messages.ui.RequestSignature;
+import finotek.global.dev.talkbank_ca.chat.messages.ui.TransferRequestSignature;
 import finotek.global.dev.talkbank_ca.chat.storage.TransactionDB;
 import finotek.global.dev.talkbank_ca.model.DBHelper;
 
@@ -75,7 +73,7 @@ public class TransferScenario implements Scenario {
             if (money >= 1000000) {
                 MessageBox.INSTANCE.addAndWait(
                         new ReceiveMessage(context.getResources().getString(R.string.dialog_chat_finger_tip_sign)),
-                        new RequestSignature()
+                        new TransferRequestSignature()
                 );
             } else {
                 if (navigateNum == 1) {
@@ -181,6 +179,8 @@ public class TransferScenario implements Scenario {
             request.setTitle("");
             request.setDescription(context.getString(R.string.dialog_chat_after_transfer, name, moneyAsString, balanceAsString));
 
+
+
             MessageBox.INSTANCE.addAndWait(request, new Done());
             step = Step.TransferDone;
         }
@@ -191,6 +191,16 @@ public class TransferScenario implements Scenario {
 
     }
 
+    public void confirmBankSelection (int balance){
+        RecoMenuRequest req = new RecoMenuRequest();
+        req.setDescription(context.getResources().getString(R.string.dialog_chat_bank_balance, balance));
+        req.addMenu(R.drawable.icon_haha, context.getResources().getString(R.string.dialog_chat_select_bank_yes), null);
+        req.addMenu(R.drawable.icon_sad, context.getResources().getString(R.string.dialog_chat_select_bank_no), null);
+        MessageBox.INSTANCE.addAndWait(
+               req
+        );
+        step = Step.BankChoice;
+    }
 
     @Override
     public void onUserSend(String msg) {
@@ -209,7 +219,7 @@ public class TransferScenario implements Scenario {
 
             case BankAnswer:
                 if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_yes))) {
-                    selectAccounts();
+                    Accounts(1);
                 } else if ((msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_no)))) {
                     RecoMenuRequest selectbank = new RecoMenuRequest();
                     selectbank.setDescription(context.getResources().getString(R.string.dialog_chat_bank_select));
@@ -225,50 +235,32 @@ public class TransferScenario implements Scenario {
                     break;
                 }
             case Question:
-
-                if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_A1))) {
-                    int balance = TransactionDB.INSTANCE.getFirstAlternativeBalance();
-                    RecoMenuRequest a1 = new RecoMenuRequest();
-                    a1.setDescription(context.getResources().getString(R.string.dialog_chat_bank_balance, balance));
-                    a1.addMenu(R.drawable.icon_haha, context.getResources().getString(R.string.dialog_chat_select_bank_yes), null);
-                    a1.addMenu(R.drawable.icon_sad, context.getResources().getString(R.string.dialog_chat_select_bank_no), null);
-                    MessageBox.INSTANCE.addAndWait(
-                            a1
+                if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_A1)))
+                {
+                    confirmBankSelection(TransactionDB.INSTANCE.getFirstAlternativeBalance());
+                    this.navigateNum = 2;
+                }
+                else if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_A2)))
+                {
+                    confirmBankSelection(TransactionDB.INSTANCE.getSecondAlternativeBalance());
+                    this.navigateNum = 3;
+                }
+                else if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_A3)))
+                {
+                    confirmBankSelection(TransactionDB.INSTANCE.getThirdAlternativeBalance());
+                    this.navigateNum = 4;
+                }
+                 else if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_cancel))){
+                    MessageBox.INSTANCE.addAndWait(new ReceiveMessage(
+                            context.getResources().getString(R.string.dialog_chat_transfer_cancel)),
+                            new RecommendScenarioMenuRequest(context)
                     );
-                    step = Step.FirstBank;
-
-                    }
-                else if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_A2))) {
-                    int balance = TransactionDB.INSTANCE.getSecondAlternativeBalance();
-                    RecoMenuRequest a2 = new RecoMenuRequest();
-                    a2.setDescription(context.getResources().getString(R.string.dialog_chat_bank_balance, balance));
-                    a2.addMenu(R.drawable.icon_haha, context.getResources().getString(R.string.dialog_chat_select_bank_yes), null);
-                    a2.addMenu(R.drawable.icon_sad, context.getResources().getString(R.string.dialog_chat_select_bank_no), null);
-                    MessageBox.INSTANCE.addAndWait(
-                            a2
-                    );
-                    step = Step.SecondBank;
-
-                } else if (msg.equals(context.getResources().getString(R.string.dialog_chat_bank_select_A3))) {
-                    int balance = TransactionDB.INSTANCE.getThirdAlternativeBalance();
-                    RecoMenuRequest a3 = new RecoMenuRequest();
-                    a3.setDescription(context.getResources().getString(R.string.dialog_chat_bank_balance, balance));
-                    a3.addMenu(R.drawable.icon_haha, context.getResources().getString(R.string.dialog_chat_select_bank_yes), null);
-                    a3.addMenu(R.drawable.icon_sad, context.getResources().getString(R.string.dialog_chat_select_bank_no), null);
-                    MessageBox.INSTANCE.addAndWait(
-                            a3
-                    );
-                    step = Step.ThirdBank;
-
-                } else {
-                    MessageBox.INSTANCE.addAndWait(
-                            new ReceiveMessage(context.getString(R.string.dialog_chat_recognize_error))
-                    );                }
+                }
                 break;
 
-            case FirstBank:
+            case BankChoice:
                 if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_yes))) {
-                    firstBank();
+                    Accounts(navigateNum);
                 }
                 else if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_no))) {
                     RecoMenuRequest selectbank = new RecoMenuRequest();
@@ -281,43 +273,9 @@ public class TransferScenario implements Scenario {
                     MessageBox.INSTANCE.addAndWait(
                             selectbank
                     );                }
-                break;
-
-            case SecondBank:
-                if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_yes))) {
-                    secondBank();
-                }
-                else if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_no))) {
-                    RecoMenuRequest selectbank = new RecoMenuRequest();
-                    selectbank.setDescription(context.getResources().getString(R.string.dialog_chat_bank_select));
-                    selectbank.addMenu(R.drawable.icon_love, context.getResources().getString(R.string.dialog_chat_bank_select_A1), null);
-                    selectbank.addMenu(R.drawable.icon_mike, context.getResources().getString(R.string.dialog_chat_bank_select_A2), null);
-                    selectbank.addMenu(R.drawable.icon_haha, context.getResources().getString(R.string.dialog_chat_bank_select_A3), null);
-                    selectbank.addMenu(R.drawable.icon_sad, context.getResources().getString(R.string.dialog_chat_bank_select_cancel), null);
-
-                    MessageBox.INSTANCE.addAndWait(
-                            selectbank
-                    );
-                }
-                break;
-            case ThirdBank:
-                if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_yes))) {
-                    thirdBank();
-                }
-                else if (msg.equals(context.getResources().getString(R.string.dialog_chat_select_bank_no))) {
-                    RecoMenuRequest selectbank = new RecoMenuRequest();
-                    selectbank.setDescription(context.getResources().getString(R.string.dialog_chat_bank_select));
-                    selectbank.addMenu(R.drawable.icon_love, context.getResources().getString(R.string.dialog_chat_bank_select_A1), null);
-                    selectbank.addMenu(R.drawable.icon_mike, context.getResources().getString(R.string.dialog_chat_bank_select_A2), null);
-                    selectbank.addMenu(R.drawable.icon_haha, context.getResources().getString(R.string.dialog_chat_bank_select_A3), null);
-                    selectbank.addMenu(R.drawable.icon_sad, context.getResources().getString(R.string.dialog_chat_bank_select_cancel), null);
-
-                    MessageBox.INSTANCE.addAndWait(
-                            selectbank
-                    );
-                    step = Step.Question;                }
                 break;
         }
+
     }
 
     @Override
@@ -333,8 +291,8 @@ public class TransferScenario implements Scenario {
         return isProceeding;
     }
 
-    private void selectAccounts() {
-        navigateNum = 1;
+    private void Accounts(int navigateNum){
+        this.navigateNum = navigateNum;
         List<Account> accounts = new ArrayList<>();
         accounts.add(new Account("어머니", "2017/01/25", "200,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), true));
         accounts.add(new Account("박예린", "2017/01/11", "100,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
@@ -346,72 +304,34 @@ public class TransferScenario implements Scenario {
             MessageBox.INSTANCE.add(new RequestSelectContact());
         }, false);
 
-        MessageBox.INSTANCE.addAndWait(
-                new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
-                new AccountList(accounts),
-                confirmRequest,
-                new RequestTransferUI(),
-                new RequestContactPermission()
-        );
-    }
-    private void firstBank() {
-        navigateNum = 2;
-        List<Account> accounts = new ArrayList<>();
-        accounts.add(new Account("어머니", "2017/01/25", "200,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), true));
-        accounts.add(new Account("박예린", "2017/01/11", "100,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
-        accounts.add(new Account("김가람", "2017/01/11", "36,200 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
-        accounts.add(new Account("김이솔", "2017/01/10", "100,000 원 " + context.getString(R.string.dialog_string_deposit).toLowerCase(), false));
-
-        ConfirmRequest confirmRequest = new ConfirmRequest();
-        confirmRequest.addInfoEvent(context.getString(R.string.dialog_contact), () -> {
-            MessageBox.INSTANCE.add(new RequestSelectContact());
-        }, false);
-
-        MessageBox.INSTANCE.addAndWait(
-                new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
-                new AccountList(accounts),
-                confirmRequest,
-                new RequestTransferUI_v1(),
-                new RequestContactPermission()
-        );
-
-    }
-
-    private void secondBank() {
-        navigateNum = 3;
-        List<Account> accounts = new ArrayList<>();
-        accounts.add(new Account("어머니", "2017/01/25", "200,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), true));
-        accounts.add(new Account("박예린", "2017/01/11", "100,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
-        accounts.add(new Account("김가람", "2017/01/11", "36,200 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
-        accounts.add(new Account("김이솔", "2017/01/10", "100,000 원 " + context.getString(R.string.dialog_string_deposit).toLowerCase(), false));
-
-        ConfirmRequest confirmRequest = new ConfirmRequest();
-        confirmRequest.addInfoEvent(context.getString(R.string.dialog_contact), () -> {
-            MessageBox.INSTANCE.add(new RequestSelectContact());
-        }, false);
-
-        MessageBox.INSTANCE.addAndWait(
-                new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
-                new AccountList(accounts),
-                confirmRequest,
-                new RequestTransferUI_v2(),
-                new RequestContactPermission()
-        );
-    }
-
-        private void thirdBank() {
-            navigateNum = 4;
-            List<Account> accounts = new ArrayList<>();
-            accounts.add(new Account("어머니", "2017/01/25", "200,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), true));
-            accounts.add(new Account("박예린", "2017/01/11", "100,000 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
-            accounts.add(new Account("김가람", "2017/01/11", "36,200 원 " + context.getString(R.string.string_transfer).toLowerCase(), false));
-            accounts.add(new Account("김이솔", "2017/01/10", "100,000 원 " + context.getString(R.string.dialog_string_deposit).toLowerCase(), false));
-
-            ConfirmRequest confirmRequest = new ConfirmRequest();
-            confirmRequest.addInfoEvent(context.getString(R.string.dialog_contact), () -> {
-                MessageBox.INSTANCE.add(new RequestSelectContact());
-            }, false);
-
+        if (navigateNum == 1) {
+            MessageBox.INSTANCE.addAndWait(
+                    new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
+                    new AccountList(accounts),
+                    confirmRequest,
+                    new RequestTransferUI(),
+                    new RequestContactPermission()
+            );
+        }
+        else if (navigateNum == 2){
+            MessageBox.INSTANCE.addAndWait(
+                    new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
+                    new AccountList(accounts),
+                    confirmRequest,
+                    new RequestTransferUI_v1(),
+                    new RequestContactPermission()
+            );
+        }
+        else if (navigateNum == 3){
+            MessageBox.INSTANCE.addAndWait(
+                    new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
+                    new AccountList(accounts),
+                    confirmRequest,
+                    new RequestTransferUI_v2(),
+                    new RequestContactPermission()
+            );
+        }
+        else if (navigateNum == 4){
             MessageBox.INSTANCE.addAndWait(
                     new ReceiveMessage(context.getString(R.string.dialog_string_select_receiver)),
                     new AccountList(accounts),
@@ -419,10 +339,11 @@ public class TransferScenario implements Scenario {
                     new RequestTransferUI_v3(),
                     new RequestContactPermission()
             );
-
         }
+    }
+
 
     private enum Step {
-        BankAsk, BankAnswer, Question, FirstBank, SecondBank, ThirdBank, Initial, TransferToSomeone, TransferByAI, TransferDone
+        BankAsk, BankAnswer, Question, BankChoice, Initial, TransferToSomeone, TransferByAI, TransferDone
     }
 }
---
+++ app/src/main/java/finotek/global/dev/talkbank_ca/user/sign/TransferSignRegisterFragment.java
@@ -0,0 +1,64 @@
+package finotek.global.dev.talkbank_ca.user.sign;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import finotek.global.dev.talkbank_ca.R;
+import finotek.global.dev.talkbank_ca.chat.scenario.TransferScenario;
+import finotek.global.dev.talkbank_ca.chat.storage.TransactionDB;
+
+/**
+ * Created by jungwon on 7/18/2017.
+ */
+
+public class TransferSignRegisterFragment extends BaseSignRegisterFragment {
+
+    @Override
+    void initView() {
+
+        String name = TransactionDB.INSTANCE.getTxName();
+        String moneyAsString = TransactionDB.INSTANCE.getTxMoney();
+
+        String temp = getResources().getString(R.string.dialog_chat_send_transfer, name, moneyAsString);
+        binding.tvInst.setText(temp);
+
+        binding.ibNext.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.btn_confirm_disable));
+    }
+
+    @Override
+    void setNextStepAction(int step) {
+        switch (step) {
+            case 2:
+                binding.tvInst.setText("");
+                binding.ibNext.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.btn_confirm));
+                break;
+        }
+    }
+
+    @Override
+    void setOnTouchCount() {
+
+        binding.drawingCanvas.setOnCanvasTouchListener(() -> {
+            if (stepCount == 1) {
+                stepSubject.onNext(++stepCount);
+            }
+
+            binding.ibSizeControl.setVisibility(View.GONE);
+        });
+
+        RxView.clicks(binding.ibNext)
+                .throttleFirst(200, TimeUnit.MILLISECONDS)
+                .subscribe(aVoid -> {
+                    if (stepCount > 1) {
+                        stepSubject.onNext(3);
+                        stepSubject.onComplete();
+                    }
+                });
+
+    }
+}

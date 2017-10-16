5909d53ae17a4edd5d2dae389fd578ab0529c2c9
이도현 <cailhuiris@gmail.com>
2017-10-16 10:03:28 +0900

    기부 시나리오 추가

--- app/src/main/java/finotek/global/dev/talkbank_ca/chat/messages/control/RecommendScenarioMenuRequest.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/chat/messages/control/RecommendScenarioMenuRequest.java
@@ -29,6 +29,8 @@ public class RecommendScenarioMenuRequest extends RecoMenuRequest {
 					addMenu(R.drawable.icon_love, context.getResources().getString(R.string.main_string_v2_login_house_loan), null);
 				if (s.equals("C"))
 					addMenu(R.drawable.icon_love, context.getResources().getString(R.string.dialog_chat_how_score_is_computed), null);
+				if (s.equals("D"))
+					addMenu(R.drawable.icon_love, context.getResources().getString(R.string.dialog_chat_donate_menu), null);
 			}
 			addMenu(R.drawable.icon_wow, context.getResources().getString(R.string.main_string_v2_login_notify_again), null);
 		} else {

2039eb7fb61eac0d33c42ec160c8a0465c534dcc
KyeongWookMa <kyeongwook.ma@gmail.com>
2017-11-01 14:01:27 +0900

    Brazil tts (#65)
    * Develop dhlee (#55)
    
    * 실제 맥락점수 조회 가능하게 끔 변경
    
    * 기부 시나리오 추가
    
    * 기부
    
    * 계좌사진에 실제 이름과 날짜 입히기
    
    * Develop ma (#56)
    
    * [기능 수정] 피노 사인 병합 중
    
    * [기능 구현]  사인 모듈 구현
    
    * Merge branch 'develop' into develop-ma
    
    # Conflicts:
    #	app/src/main/java/finotek/global/dev/talkbank_ca/chat/ChatActivity.java
    #	app/src/main/java/finotek/global/dev/talkbank_ca/user/sign/BaseSignRegisterFragment.java
    #	app/src/main/java/finotek/global/dev/talkbank_ca/user/sign/HiddenSignFragment.java
    #	finosign/build.gradle
    #	finosign/src/main/java/d2r/checksign/lib/FinoSign.java
    
    * add chat image view
    
    * 신분증 OCR 기능 완성
    
    * [버그 수정] 사인 검증 버그 수정
    
    * [버그 수정] 리소스 버그 수정
    
    * 브라질

--- app/src/main/java/finotek/global/dev/talkbank_ca/chat/ChatActivity.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/chat/ChatActivity.java
@@ -124,6 +124,7 @@ import finotek.global.dev.talkbank_ca.util.ChatLocationListener;
 import finotek.global.dev.talkbank_ca.util.ContextAuthPref;
 import finotek.global.dev.talkbank_ca.util.Converter;
 import finotek.global.dev.talkbank_ca.util.KeyboardUtils;
+import globaldev.finotek.com.aws_tts.TTSPlayer;
 import globaldev.finotek.com.logcollector.Finopass;
 import globaldev.finotek.com.logcollector.api.score.BaseScoreParam;
 import globaldev.finotek.com.logcollector.api.score.ContextScoreResponse;
@@ -163,7 +164,7 @@ public class ChatActivity extends AppCompatActivity {
 	private View transferView = null;
 
 	private MainScenario_v2 mainScenario;
-
+	private TTSPlayer ttsPlayer;
 	private CapturePicFragment capturePicFragment;
 	private SignRegisterFragment signRegistFragment;
 	private SignValidationFragment transferSignRegistFragment;
@@ -229,6 +230,8 @@ public class ChatActivity extends AppCompatActivity {
 		// initialize score receiver
 		receiveScore();
 
+		ttsPlayer = new TTSPlayer(this);
+
 		// register ocr listener
 		LibraryInterface.registerOcrResultLinstener(ocrResultListener);
 	}
@@ -241,6 +244,12 @@ public class ChatActivity extends AppCompatActivity {
 
 	private void onNewMessageUpdated(Object msg) {
 		boolean isContextAuthAgreed = getSharedPreferences("prefs", Context.MODE_PRIVATE).getBoolean(getString(R.string.splash_is_auth_agree), false);
+
+		if (msg instanceof ReceiveMessage) {
+			ReceiveMessage sendMessage = (ReceiveMessage) msg;
+			String message = sendMessage.getMessage();
+			ttsPlayer.requestPollyVoice(message);
+		}
 
 		if (msg instanceof ContextTotal && isContextAuthAgreed) {
             fetchContextLogData(ContextLogType.total);
@@ -268,6 +277,7 @@ public class ChatActivity extends AppCompatActivity {
 		if (msg instanceof RequestTakeAnotherIDCard) {
 			this.prepareForFullScreen();
 			populateCaptureView();
+
 		}
 
 		if (msg instanceof RequestRemoteCall) {
@@ -975,6 +985,7 @@ public class ChatActivity extends AppCompatActivity {
 	}
 
 	private void startOcrModule() {
+
 		OcrParam ocrParam = new OcrParam();
 		//촬영 시간
 		ocrParam.setRetrytime(15000);
@@ -987,4 +998,5 @@ public class ChatActivity extends AppCompatActivity {
 
 		LibraryInterface.startOcr(this, "", "", ocrParam);
 	}
+
 }
---
+++ aws_tts/src/main/java/globaldev/finotek/com/aws_tts/TTSPlayer.java
@@ -0,0 +1,132 @@
+package globaldev.finotek.com.aws_tts;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.util.Log;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.polly.AmazonPollyPresigningClient;
+import com.amazonaws.services.polly.model.DescribeVoicesRequest;
+import com.amazonaws.services.polly.model.DescribeVoicesResult;
+import com.amazonaws.services.polly.model.OutputFormat;
+import com.amazonaws.services.polly.model.SynthesizeSpeechPresignRequest;
+import com.amazonaws.services.polly.model.Voice;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import io.reactivex.Observable;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by magyeong-ug on 2017. 11. 1..
+ */
+
+public class TTSPlayer {
+
+	private static final String COGNITO_POOL_ID = "ap-northeast-2:02da37b8-e554-430b-8a2b-c9ae56f992d0";
+	private static final Regions MY_REGION = Regions.AP_NORTHEAST_2;
+	private MediaPlayer mediaPlayer;
+	private AmazonPollyPresigningClient client;
+	private Context context;
+
+	public TTSPlayer(Context appContext) {
+		this.context = appContext;
+		this.mediaPlayer = new MediaPlayer();
+		initPollyClient();
+	}
+
+	private void initPollyClient() {
+		CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+				context,
+				COGNITO_POOL_ID,
+				MY_REGION
+		);
+
+		// Create a client that supports generation of presigned URLs.
+		client = new AmazonPollyPresigningClient(credentialsProvider);
+	}
+
+	public void requestPollyVoice(final String textToRead) {
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+				// Create describe voices request.
+				DescribeVoicesRequest describeVoicesRequest = new DescribeVoicesRequest();
+
+				DescribeVoicesResult describeVoicesResult = null;
+				try {
+					// Synchronously ask the Polly Service to describe available TTS voices.
+					describeVoicesResult = client.describeVoices(describeVoicesRequest);
+				} catch (RuntimeException e) {
+					Log.e(TAG, "Unable to get available voices. " + e.getMessage());
+				}
+
+				// Get list of voices from the result.
+				List<Voice> voices = describeVoicesResult.getVoices();
+
+				// Create speech synthesis request.
+				SynthesizeSpeechPresignRequest synthesizeSpeechPresignRequest =
+						new SynthesizeSpeechPresignRequest()
+								.withText(textToRead)
+								.withVoiceId(voices.get(0).getId())
+								.withOutputFormat(OutputFormat.Mp3);
+
+				// Get the presigned URL for synthesized speech audio stream.
+				URL presignedSynthesizeSpeechUrl =
+						client.getPresignedSynthesizeSpeechUrl(synthesizeSpeechPresignRequest);
+
+				Log.i(TAG, "Playing speech from presigned URL: " + presignedSynthesizeSpeechUrl);
+
+				// Create a media player to play the synthesized audio stream.
+
+				setupNewMediaPlayer();
+
+				mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+				try {
+					// Set media player's data source to previously obtained URL.
+					mediaPlayer.setDataSource(presignedSynthesizeSpeechUrl.toString());
+				} catch (IOException e) {
+					Log.e(TAG, "Unable to set data source for the media player! " + e.getMessage());
+				}
+
+				// Start the playback asynchronously (since the data source is a network stream).
+				mediaPlayer.prepareAsync();
+			}
+		}).start();
+
+
+	}
+
+	private void setupNewMediaPlayer() {
+		mediaPlayer = new MediaPlayer();
+		mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+				setupNewMediaPlayer();
+			}
+		});
+		mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+				mp.start();
+			}
+		});
+		mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+			@Override
+			public boolean onError(MediaPlayer mp, int what, int extra) {
+				return false;
+			}
+		});
+
+	}
+}

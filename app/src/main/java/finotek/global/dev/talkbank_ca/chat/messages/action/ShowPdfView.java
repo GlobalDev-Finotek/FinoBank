ad8f8fe2aed1d32814d77fab18237908a387d4de
이도현 <cailhuiris@gmail.com>
2017-12-06 11:10:39 +0900

    Revert "[기능 수정] FinoSign 백엔드 버젼으로 변경"
    This reverts commit aaa29217dab5e19c82ae88bb22994801e96c0acb.

--- app/src/main/java/finotek/global/dev/talkbank_ca/chat/messages/action/ShowPdfView.java
+++ app/src/main/java/finotek/global/dev/talkbank_ca/chat/messages/action/ShowPdfView.java
@@ -2,7 +2,6 @@ package finotek.global.dev.talkbank_ca.chat.messages.action;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -10,9 +9,4 @@ public class ShowPdfView {
 	String title;
 	String pdfAsset;
 	int step;
-
-	public ShowPdfView(String title, String pdfAsset) {
-		this.title = title;
-		this.pdfAsset = pdfAsset;
-	}
 }
